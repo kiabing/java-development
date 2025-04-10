9d866e9d0ac0f616703c36bf5c0c56b73527e2c3
kiabing <deinellbing@gmail.com>
2025-04-10 16:41:30 -0400

    Initial commit

---
+++ workbook-1/com.pluralsight/src/main/java/shoppinglist.java
@@ -0,0 +1,15 @@
+public class shoppinglist {
+
+    public static void main(String[] args) {
+        System.out.println("greens");
+        System.out.println("water");
+        System.out.println("juice");
+        System.out.println("coffee");
+        System.out.println("sugar");
+        System.out.println("lemon");
+        System.out.println("oysters");
+        System.out.println("shrimp");
+        System.out.println("tumeric");
+        System.out.println("oldbay");
+    }
+}
---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/Helloworld.java
@@ -0,0 +1,6 @@
+package com.pluralsight;
+
+public class Helloworld {
+   public static void main(String[] args) {
+       System.out.println("Hello Everyone I am Raymond and I love potatoes");
+       System.out.println("Raymond we need a break!");
